111095d430a0737f758e6d286b243f2ccd1cdefc
Mustafa Arif Sisman <mustafaarifsisman@gmail.com>
2019-03-26 15:21:03 +0300

    cosine test

--- similarity.py
+++ similarity.py
@@ -1,3 +1,5 @@
+import xlrd
+
 from collections import Counter
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
@@ -22,17 +24,37 @@ def get_vectors(*strs):
     return vectorizer.transform(text).toarray()
 
 
-def main():
-    x = get_jaccard(
-        "hello",
-        "hello world!")
-    y = get_cosine_sim("hello",
-                       "hello world!"
-			"asdhello"
-)
+# Jaccard helper
 
-    print("jaccard similarity is", x)
-    print("cosine similarity is", y)
+# Cosine helper
+
+
+# Program extracting first column
+def get_data():
+    data = []
+    loc = "hwdata1.xls"
+
+    wb = xlrd.open_workbook(loc)
+    sheet = wb.sheet_by_index(0)
+    sheet.cell_value(0, 0)
+
+    for i in range(sheet.nrows):
+        str = sheet.cell_value(i, 1)
+        data.append(str)
+    # texts = [[word.lower() for word in text.split()] for text in data]
+    return data
+
+
+def main():
+    # x = get_jaccard("hello","hello world!")
+    # y = get_cosine_sim("hello","hello world!","asdhello")
+    # print("jaccard similarity is", x)
+    # print("cosine similarity is\n", y)
+
+    data = get_data()
+    z = get_cosine_sim("Apple", "Banana", "Cherry", "Apple")
+
+    print("cosine similarity of the data's", z)
 
 
 if __name__ == '__main__':
